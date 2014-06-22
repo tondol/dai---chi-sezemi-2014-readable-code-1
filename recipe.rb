7afdac7171d1771d46673674d6c212e0851d89d5
daichi <daipanchi@gmail.com>
2014-06-22 11:59:59 +0900

    Create main method

--- recipe.rb
+++ recipe.rb
@@ -1,5 +1,22 @@
 #! /usr/bin/ruby
 
+def main
+  if ARGV[0].nil?
+    puts 'usage: ruby recipe.rb fine_name'
+  else
+    recipe_data = open_file(ARGV[0])
+    if ARGV[1].nil?
+      recipe_data.each_with_index do |line, i|
+        print_title_with_id(i+1, line)
+      end
+    else
+      recipe_id = ARGV[1].to_i
+      print_title_with_id(recipe_id, recipe_data.to_a[recipe_id-1])
+    end
+    recipe_data.close
+  end
+end
+
 def print_title(title)
   puts title
 end
@@ -12,17 +29,4 @@ def print_title_with_id(id, title)
   puts "#{id}: #{title}"
 end
 
-if ARGV[0].nil?
-  puts 'usage: ruby recipe.rb fine_name'
-else
-  recipe_data = open_file(ARGV[0])
-  if ARGV[1].nil?
-    recipe_data.each_with_index do |line, i|
-      print_title_with_id(i+1, line)
-    end
-  else
-    recipe_id = ARGV[1].to_i
-    print_title_with_id(recipe_id, recipe_data.to_a[recipe_id-1])
-  end
-  recipe_data.close
-end
+main
