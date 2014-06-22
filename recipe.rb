076b58640947ed18f750810cfd2a7ff690f62a8d
daichi <daipanchi@gmail.com>
2014-06-22 11:59:33 +0900

    spec6: Display only a recipe with id

--- recipe.rb
+++ recipe.rb
@@ -8,14 +8,21 @@ def open_file(file_name)
   File.open(file_name, 'r')
 end
 
-if ARGV[0] == nil
+def print_title_with_id(id, title)
+  puts "#{id}: #{title}"
+end
+
+if ARGV[0].nil?
   puts 'usage: ruby recipe.rb fine_name'
 else
   recipe_data = open_file(ARGV[0])
-
-  recipe_data.each_with_index do |line, i|
-    puts "#{i+1}: #{line}"
+  if ARGV[1].nil?
+    recipe_data.each_with_index do |line, i|
+      print_title_with_id(i+1, line)
+    end
+  else
+    recipe_id = ARGV[1].to_i
+    print_title_with_id(recipe_id, recipe_data.to_a[recipe_id-1])
   end
-
   recipe_data.close
 end
