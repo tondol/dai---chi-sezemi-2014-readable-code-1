6a59e65cebbad6871fc5a7447eb1a03fed7c60b1
daichi <daipanchi@gmail.com>
2014-06-22 11:37:22 +0900

    spec5: Add id when printing

--- recipe.rb
+++ recipe.rb
@@ -8,10 +8,14 @@ def open_file(file_name)
   File.open(file_name, 'r')
 end
 
-recipe_data = open_file(ARGV[0])
+if ARGV[0] == nil
+  puts 'usage: ruby recipe.rb fine_name'
+else
+  recipe_data = open_file(ARGV[0])
 
-recipe_data.each do |line|
-  puts line
+  recipe_data.each_with_index do |line, i|
+    puts "#{i+1}: #{line}"
+  end
+
+  recipe_data.close
 end
-
-recipe_data.close
